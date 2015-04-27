d0c30c7115dc8831567b4dfc460a77f4c06e932c
Antonio Benitez <abenitez@litografiaroses.com>
2015-04-27 21:32:32 +0200

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,9 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = 'Welcome'
+		flash.now[:alert] = 'My birthday is soon.'
+		#flash[:warning] = 'My birthday is soon2∫.'
 	end
 
 end
