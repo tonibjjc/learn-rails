3b97a23e803b1419e402e2f598ec4131ea3db9c1
Antonio Benitez <abenitez@litografiaroses.com>
2015-04-28 18:57:19 +0200

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,6 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
-		flash.now[:notice] = 'Welcome'
-		flash.now[:alert] = 'My birthday is soon.'
-		#flash[:warning] = 'My birthday is soon2∫.'
 	end
 
 end
